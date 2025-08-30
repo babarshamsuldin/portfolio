6f36b651aa23686cf748db7f2d4ed6299abbda6a
babar shamsudin <babarshamsuldin@gmail.com>
2025-08-30 04:08:44 -0700

    navbar responsive hu gi ha or website to mobile ab home page start hu rha ha

--- src/component/Nav/Navbar.jsx
+++ src/component/Nav/Navbar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Navbar.css"
 import {Link} from "react-scroll"
 
 const Navbar = () => {
+    let menu = useRef()
+    let mobile = useRef()
+    
   return (
     <>
       <nav>
         <h1>PORTFOLIO</h1>
 
-        <ul>
+        <ul className='desktopmenu'>
+        <Link to='home' smooth={true} duration={500} activeClass='active' spy={true} ><li>Home</li></Link>
+        <Link to='about' spy={true} smooth={true} duration={500} activeClass='active'><li>About</li></Link>
+        <Link to='project' spy={true} smooth={true} duration={500} activeClass='active'><li>Project</li></Link>
+        <Link to='contact' spy={true} smooth={true} duration={500} activeClass='active'><li>Contact</li></Link>
+        </ul>
+
+        <div className="hamburger" ref={menu} onClick={()=>{
+            mobile.current.classList.toggle("activemobile")
+            menu.current.classList.toggle("activeham")
+        }}>
+            <div className="ham"></div>
+            <div className="ham"></div>
+            <div className="ham"></div>
+        </div>
+
+        <ul className='mobilemenu' ref={mobile}>
         <Link to='home' smooth={true} duration={500} activeClass='active' spy={true} ><li>Home</li></Link>
         <Link to='about' spy={true} smooth={true} duration={500} activeClass='active'><li>About</li></Link>
         <Link to='project' spy={true} smooth={true} duration={500} activeClass='active'><li>Project</li></Link>
