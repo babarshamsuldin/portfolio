f293dab6609500459030c8e8ba0c738891185c9d
babar shamsudin <babarshamsuldin@gmail.com>
2025-08-30 05:48:26 -0700

    home page complete hu gia ha or responsive bhi kar diya ha

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import "./Home.css"
+import man from "..//../assets/man.png"
 
 const Home = () => {
   return (
     <div id='home'>
       <div className="lefthome">
-        
+        <div className="homedetails">
+          <div className="line1">I'M</div>
+          <div className="line2">Babar shamsudin</div>
+          <div className="line3">Data sacintist</div>
+          <button>Hire Me</button>
+        </div>
       </div>
 
-      <div className="righthome"></div>
+      <div className="righthome">
+        <img src={man} alt="" />
+      </div>
     </div>
   )
 }
