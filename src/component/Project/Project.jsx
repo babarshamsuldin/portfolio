f607aa3a6844883be87e0b845aff497c53ebf634
babar shamsudin <babarshamsuldin@gmail.com>
2025-08-30 21:56:34 -0700

    project complete hu gia ha or responsive bhi hu gia ha

--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Project.css"
+import Card from "../Card/Card"
+import vs from "../../assets/va.png"
+import fw from "../../assets/fw.png"
+import ise from "../../assets/ise.png"
+import va from "../../assets/va.png"
+import tti from "../../assets/tti.png"
 
 const Project = () => {
   return (
     <div id='project'>
-      
+      <h1 id='para'>2 Year Experince In Projects</h1>
+      <div className="slider">
+        <Card title={"Virtcul Assistant"} image={vs}/>
+        <Card title={"AI Power Fitness Website"} image={fw}/>
+        <Card title={"AI Chatbot"} image={ise}/>
+        <Card title={"Virtcul Assistant"} image={va}/>
+        <Card title={"Virtcul Assistant"} image={tti}/>
+      </div>
     </div>
   )
 }
