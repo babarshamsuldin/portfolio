2cf4a597d11b9c2d2384782794c6b5566c030241
babar shamsudin <babarshamsuldin@gmail.com>
2025-08-30 23:11:47 -0700

    contact page complete hu gia ha , ab animation start kar rha hun

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./Contact.css"
+import contact from "../../assets/contact.png"
 
 const Contact = () => {
   return (
     <div id='contact'>
-      
+      <div className="leftcontact">
+        <img src={contact} alt="" />
+      </div>
+      <div className="rightcontact">
+        <form action="https://formspree.io/f/xrbakgzp" method='POST'>
+          <input type="text" name='Username' placeholder='Name' />
+          <input type="text" name='Email' placeholder='Email' />
+          <textarea name="Message" id="textarea" placeholder='Message Me'></textarea>
+          <input type="submit" name="Submit" id="btn" value={"Submit"} />
+        </form>
+      </div>
     </div>
   )
 }
