1e43d3167f033a6de3e51671598dffb9d96924ed
babar shamsudin <babarshamsuldin@gmail.com>
2025-08-31 00:17:50 -0700

    project page ki bhi animation complete hu gi ha

--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -6,8 +6,43 @@ import fw from "../../assets/fw.png"
 import ise from "../../assets/ise.png"
 import va from "../../assets/va.png"
 import tti from "../../assets/tti.png"
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger)
 
 const Project = () => {
+  useGSAP(()=>{
+    gsap.from("#para",{
+      y: 100,
+      duration: 1,
+      opacity:0,
+      stagger:1,
+      scrollTrigger:{
+        trigger: "#para",
+        scroll: "body",
+        scrub:2,
+        markers: true,
+        start:"top 80%",
+        end:"end 30%"
+      }
+    })
+    gsap.from(".slider",{
+      y: 100,
+      duration: 1,
+      opacity:0,
+      stagger:1,
+      scrollTrigger:{
+        trigger: "#para",
+        scroll: "body",
+        scrub:2,
+        markers: true,
+        start:"top 80%",
+        end:"end 30%"
+      }
+    })
+  })
+  
   return (
     <div id='project'>
       <h1 id='para'>2 Year Experince In Projects</h1>
